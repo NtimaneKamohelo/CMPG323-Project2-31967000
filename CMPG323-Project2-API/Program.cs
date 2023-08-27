35dac4ca187de6910f292c913f71d0a43e268b24
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-08-27 22:36:05 +0200

    scaffolded the database into the project

--- CMPG323-Project2-API/Program.cs
+++ CMPG323-Project2-API/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using CMPG323_Project2_API.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
